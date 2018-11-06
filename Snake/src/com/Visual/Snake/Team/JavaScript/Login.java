f7c11cf090cccd59485ee57ef05f26e96da32355
Nicolas <nmanograsso@gmail.com>
2018-11-06 20:28:07 -0300

    add password and a user

--- Snake/src/com/Visual/Snake/Team/JavaScript/Login.java
+++ Snake/src/com/Visual/Snake/Team/JavaScript/Login.java
@@ -1,10 +1,6 @@
 package com.Visual.Snake.Team.JavaScript;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Frame;
-import java.awt.Window;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,11 +9,17 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class Login extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField nameTextField;
+	private JTextField passTextField;
 
 	/**
 	 * Launch the application.
@@ -40,33 +42,56 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 339, 176);
+		setBounds(100, 100, 300, 250);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 		
-		JLabel lblNewLabel = new JLabel("Name");
-		lblNewLabel.setBounds(87, 26, 143, 14);
+		JLabel lblNewLabel = new JLabel("Usuario");
+		lblNewLabel.setBounds(75, 26, 143, 14);
 		contentPane.add(lblNewLabel);
 		
 		nameTextField = new JTextField();
-		nameTextField.setBounds(87, 45, 143, 20);
+		nameTextField.setBounds(75, 41, 143, 20);
 		contentPane.add(nameTextField);
 		nameTextField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Start");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton loginBtn = new JButton("Ingresar");
+		loginBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//System.out.println(nameTextField.getText());
-				if(!nameTextField.getText().isEmpty()) {					
+				if(nameTextField.getText().equals("Javascript") && passTextField.getText().equals("123")) {					
 					new Menu();
 					dispose();
 				}
+				else {
+					passTextField.setText("");
+					System.out.println("Contraseña o usuario invalido");
+				}
 			}
 		});
-		btnNewButton.setBounds(87, 77, 143, 23);
+		
+		loginBtn.setBounds(75, 126, 143, 23);
+		contentPane.add(loginBtn);
+		
+		JLabel passwordTextField = new JLabel("Contraseña");
+		passwordTextField.setBounds(75, 73, 143, 14);
+		contentPane.add(passwordTextField);
+		
+		passTextField = new JTextField();
+		passTextField.setToolTipText("");
+		passTextField.setColumns(10);
+		passTextField.setBounds(75, 88, 143, 20);
+		contentPane.add(passTextField);
+		
+		JButton btnNewButton = new JButton("Olvide la contraseña");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("...");
+			}
+		});
+		btnNewButton.setBounds(137, 193, 157, 29);
 		contentPane.add(btnNewButton);
 	}
 }
